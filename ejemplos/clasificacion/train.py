cb7423333866611498ba110d01a0bdba3d152a23
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2025-07-16 20:06:53 +0000

    "Creacion de una clasificacion"

---
+++ ejemplos/clasificacion/train.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+data = {
+    "tiempo_minutos" : [5, 8, 12, 2, 20, 15, 3], 
+    "paginas_visitadas" : [3, 5, 7, 1, 10, 8, 2], 
+    "compro" : [0, 0, 1, 0, 1, 1, 0]
+}
+
+df = pd.DataFrame(data)
+
+X = df[["tiempo_minutos", "paginas_visitadas"]]
+y = df["compro"]
+
+X_train, X_test, y_train, y_test, = train_test_split(X, y) 
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(X_test)
+print(y_pred)
